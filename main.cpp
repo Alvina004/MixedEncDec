8555bfcbd0db3aa25f4c740a0ff893497b4d80d6
Alvina004 <gabrielyanalvina2@gmail.com>
2024-11-11 21:29:08 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,59 +1,24 @@
 #include <iostream>
-#include <cstring>
-#include "RLE.h"
-#include "RowCol.h"
-
-using namespace std;
+#include "RowColRle.h"
 
 int main() {
-    unsigned char src[1024], rowColEncrypted[1024], rleEncrypted[1024], decrypted[1024];
-    unsigned int size, rowColEncryptedSize, rleEncryptedSize;
+    RowColRle crypto; 
+    std::string text;
+    unsigned int key;
 
-    cout << "Enter a string to encrypt: ";
-    cin.getline((char*)src, 1024);
+    std::cout << "Enter the text to encrypt: ";
+    std::getline(std::cin, text);
 
-      cout << "Enter the RowCol key (number of columns): ";
-    unsigned int rowColKey;
-    cin >> rowColKey;
+    std::cout << "Enter the number of columns for RowCol encryption (key): ";
+    std::cin >> key;
 
-      rowcol rowColObj;
-    rowColObj.SetKey(rowColKey);
-    if (rowColObj.Encrypt(src, strlen((const char*)src), rowColEncrypted, rowColEncryptedSize)) {
-        cout << "Failed to encrypt with RowCol cipher...\n";
-        return -1;
-    }
-    rowColEncrypted[rowColEncryptedSize] = '\0';
-    cout << "RowCol Encrypted string: " << rowColEncrypted << endl;
+    crypto.setKey(key);
 
-     RLE rle;
-    if (rle.Encrypt(rowColEncrypted, rowColEncryptedSize, rleEncrypted, rleEncryptedSize)) {
-        cout << "Failed to compress with RLE...\n";
-        return -2;
-    }
-    rleEncrypted[rleEncryptedSize] = '\0';
-    cout << "RLE Compressed string: " << rleEncrypted << endl;
+    std::string encryptedText = crypto.encrypt(text);
+    std::cout << "Encrypted Text: " << encryptedText << std::endl;
 
-     unsigned char rleDecrypted[1024];
-    unsigned int rleDecryptedSize;
-    if (rle.Decrypt(rleEncrypted, rleEncryptedSize, rleDecrypted, rleDecryptedSize)) {
-        cout << "Failed to decrypt with RLE...\n";
-        return -3;
-    }
-    rleDecrypted[rleDecryptedSize] = '\0';
-    cout << "RLE Decompressed string: " << rleDecrypted << endl;
-
-      if (rowColObj.Decrypt(rleDecrypted, rleDecryptedSize, decrypted, size)) {
-        cout << "Failed to decrypt with RowCol cipher...\n";
-        return -4;
-    }
-    decrypted[size] = '\0';
-    cout << "Decrypted string: " << decrypted << endl;
-
-      if (strcmp((const char*)src, (const char*)decrypted) == 0) {
-        cout << "Success! Decryption is correct." << endl;
-    } else {
-        cout << "Decryption failed." << endl;
-    }
+    std::string decryptedText = crypto.decrypt(encryptedText);
+    std::cout << "Decrypted Text: " << decryptedText << std::endl;
 
     return 0;
 }
